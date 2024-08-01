a4b6ec366deacc1d7081c93b07224c7246592d20
Robert Corrado <82347372+robbycorrs23@users.noreply.github.com>
2024-08-01 09:26:28 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+      <AboutSection />
+    </div>
+  );
+}
+
+export default App;
